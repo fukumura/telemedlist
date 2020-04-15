f2d3916cdef3f7e66f5d8d855ff347b32ccc7e31
fukumura <fukumura.akinori@gmail.com>
2020-04-15 22:03:01 +0900

    og:urlの調整

--- pages/prefs/_id.vue
+++ pages/prefs/_id.vue
@@ -24,7 +24,6 @@
 import prefs from '@/data/prefs.json'
 import Pref from '~/components/Pref.vue'
 import Meta from '~/assets/mixins/meta'
-
 export default {
   components: {
     Pref
@@ -33,6 +32,7 @@ export default {
   data () {
     return {
       Pref,
+      prefId: this.$route.params.id,
       prefName: prefs[Number(this.$route.params.id) - 1].name
     }
   },
@@ -40,7 +40,9 @@ export default {
     return {
       title: this.prefName + 'のオンライン診療医療機関',
       meta: [
+        { hid: 'og:url', name: 'og:url', content: process.env.baseUrl + '/prefs/' + this.prefId },
         { hid: 'description', name: 'description', content: this.prefName + 'のオンライン診療の初診解禁で初診・再診OKの病院・クリニックを検索できます。オンライン診療実施医療機関リストの公開データを元に日々更新しているサイトです。' },
+        { hid: 'og:title', name: 'og:title', content: this.prefName + 'のオンライン診療検索。' },
         { hid: 'og:description', name: 'og:description', content: this.prefName + 'のオンライン診療の初診解禁で初診・再診OKの病院・クリニックを検索できます。' }
       ]
     }
