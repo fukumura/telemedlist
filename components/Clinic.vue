47812ced96d3a00cbd3d957a8418c9f1c99d470f
kzhrk <info@kzhrk.com>
2020-04-20 22:09:40 +0900

    電話番号を`a[href="tel:***"]`でラップ

--- components/Clinic.vue
+++ components/Clinic.vue
@@ -41,7 +41,7 @@
         </v-list-item-icon>
         <v-list-item-content>
           <v-list-item-subtitle>電話番号</v-list-item-subtitle>
-          <v-list-item-title>{{ clinic.tel }}</v-list-item-title>
+          <v-list-item-title><a v-if="clinic.tel" :href="`tel:${clinic.tel}`">{{ clinic.tel }}</a></v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
